9fcaab032519ba9586748a03726bf9a1876d4220
jiangjunshen <jiangjunshen@cai-inc.com>
2019-08-22 13:40:39 +0800

    增加short，byte，int，float，double，bigDecimal的转换，转为number，char转为string

--- src/com/qbb/util/TypeUtil.java
+++ src/com/qbb/util/TypeUtil.java
@@ -8,10 +8,17 @@ public class TypeUtil {
     
     public static String transform(String before) {
         String lower = before.toLowerCase();
-        if ("long".equals(lower)) {
+        if ("long".equals(lower)
+                || "bigdecimal".equals(lower)
+                || "int".equals(lower)
+                || "byte".equals(lower)
+                || "short".equals(lower)
+                || "double".equals(lower)
+                || "float".equals(lower)) {
             return "number";
         }
-        if ("date".equals(lower)) {
+        if ("date".equals(lower)
+                || "char".equals(lower)) {
             return "string";
         }
         return lower;
